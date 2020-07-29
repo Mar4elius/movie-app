cde0aa0a202290a15502eda7553349926ee5b91a
Mar4elius <marauman@gmail.com>
2020-07-28 21:47:34 -0600

    Setting up tailwind

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-red">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -18,6 +18,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+        <div className="ml-6 pt-1">
+          <h1 className="text-2xl text-blue-700 leading-tight">
+            Tailwind and Create React App
+          </h1>
+          <p className="text-base text-gray-700 leading-normal">
+            Building apps together
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    'src/assets/**/*.js',
+    'src/assets/**/*.jsx',
+    'src/assets/**/*.ts',
+    'src/assets/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
