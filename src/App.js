4e5a182c9b3cab7f5b7c438c2a785d8ca6cbabcd
Mar4elius <marauman@gmail.com>
2020-07-30 21:57:19 -0600

    Remove unneeded files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <div className="App">
-      <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-        <div className="ml-6 pt-1">
-          <h1 className="text-2xl text-blue-700 leading-tight">
-            Tailwind and Create React App
-          </h1>
-          <p className="text-base text-gray-700 leading-normal">
-            Building apps together
-          </p>
-        </div>
+      <div className="w-full">
+        <Layout />
       </div>
     </div>
   )
