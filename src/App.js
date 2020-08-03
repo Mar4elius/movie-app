b65ac05d45404f21a9ed10a8328a4b788621a415
Mar4elius <marauman@gmail.com>
2020-08-03 15:31:17 -0600

    Add font awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import Layout from './components/Layout'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faUser,
+  faVideo,
+  faLayerGroup,
+  faHeart,
+  faRandom,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faVideo, faLayerGroup, faHeart, faRandom)
 
 function App() {
   return (
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Navigation() {
+  const icons = [
+    {
+      icon: 'user',
+      name: 'Profile',
+    },
+    { icon: 'video', name: 'My Video' },
+    { icon: 'layer-group', name: 'Collection' },
+    {
+      icon: 'heart',
+      name: 'Favorites',
+    },
+    { icon: 'random', name: 'Random' },
+  ]
   return (
     <div className="w-1/5 bg-gray-200 h-full">
-      <ul className="text-center ">
-        <li className="m-5 tracking-widest">Profile</li>
-        <li className="m-5 tracking-widest">My Movies</li>
-        <li className="m-5 tracking-widest">Collections</li>
-        <li className="m-5 tracking-widest">Favorites</li>
-        <li className="m-5 tracking-widest">Random List</li>
+      <ul>
+        {icons.map(icon => {
+          return (
+            <li className="m-5 flex align-baseline justify-center">
+              <div className="flex justify-start">
+                <FontAwesomeIcon icon={icon.icon} size="2x" />
+              </div>
+              <div className="flex justify-center">
+                <p className="tracking-widest mx-5">{icon.name}</p>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
