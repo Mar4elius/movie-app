ad037d144d5c16e00cc6984985190b09ee35b8b1
Mar4elius <marauman@gmail.com>
2020-08-12 22:05:07 -0600

    Create routes js file. Jsconfig for using absolute path for import files

---
+++ src/assets/data/routes.js
@@ -0,0 +1,42 @@
+import Main from 'components/pages/Main'
+
+const routes = [
+  {
+    icon: 'igloo',
+    name: 'Home',
+    path: '/',
+    component: Main,
+  },
+  {
+    icon: 'user',
+    name: 'Profile',
+    path: '/user',
+    component: 'Profile',
+  },
+  {
+    icon: 'video',
+    name: 'My Video',
+    path: '/my-video',
+    component: 'MyVideo',
+  },
+  {
+    icon: 'layer-group',
+    name: 'Collections',
+    path: '/my-collections',
+    component: 'MyCollections',
+  },
+  {
+    icon: 'heart',
+    name: 'Favorites',
+    path: 'favorite',
+    component: 'Favorites',
+  },
+  {
+    icon: 'random',
+    name: 'Random',
+    path: '/random',
+    component: 'Random',
+  },
+]
+
+export default routes
